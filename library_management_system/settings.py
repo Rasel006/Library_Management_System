cabda5d5dc6d00c9578311986d1960b3f9b99b12
Rasel006 <rizurasel.cse@gmail.com>
2024-01-27 02:28:24 +0600

    changes done

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -31,8 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://library-management-yv5a.onrender.com','https://*.127.0.0.1']
-
+CSRF_TRUSTED_ORIGINS = ['https://library-management-v2kh.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
