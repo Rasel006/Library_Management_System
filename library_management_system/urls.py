ad01cfcd119f0225d3eecf17cd5cfae1e9d6ef22
Rasel006 <rizurasel.cse@gmail.com>
2024-01-27 05:36:35 +0600

    updated logout

--- library_management_system/urls.py
+++ library_management_system/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from core.views import HomeView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +12,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('transactions/', include('transactions.urls')),
     path('books/', include('book.urls')),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
