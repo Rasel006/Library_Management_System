ff7875344aa6b165170831094399c0b750fa3036
Rasel006 <rizurasel.cse@gmail.com>
2024-01-28 02:04:53 +0600

    changes allowed host

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://library-management-v2kh.onrender.com','https://*.127.0.0.1']
 
 
